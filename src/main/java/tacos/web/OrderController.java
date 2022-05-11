6de5e53d43ec27479750f7dc6e1f400e669c4fcd
mthsps <dmatheusps@gmail.com>
2022-05-11 19:06:23 -0300

    Chapter 02: Developing web applications

---
+++ src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,36 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.model.TacoOrder;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+
+
+}
